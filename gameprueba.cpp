c6176db47320ea75484c51348fc6dd7ccf9a3316
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-09-13 21:44:48 -0500

    2 programas simples

---
+++ gameprueba.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int main(){
+	int vida = 100, evida = 100, rnd, rnd2;
+	srand(time(NULL));
+	
+	while(vida >= 0 || evida >= 0){
+		rnd = 1+rand()%(11-1);
+		cout<<vida<<"  "<<evida<<endl;
+		rnd2 = 1+rand()%(3-1);
+		if(rnd2 == 2){
+			evida -= rnd;
+		}
+		else{
+			vida -= rnd;
+		}
+
+
+	}
+	return 0;
+}
---
+++ tarea2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"Ingresa un numero: ";cin>>num;
+	cout<<num<<" en numero positivo y negativo:\n"<<num*-1<<" "<<num;
+	return 0;
+}
